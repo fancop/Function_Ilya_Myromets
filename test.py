6c7d4342fc1c3979584edb2d5164d144c8bd3cc8
fancop <113712008+fancop@users.noreply.github.com>
2022-12-20 19:45:25 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from main import *
 
-player = make_hero(name="Вася Питонов", inventory=["зелье"], hp_now=100, money=1000)
+player = make_hero(
+    name="Вася Питонов",
+    inventory=["зелье"],
+    hp_now=100,
+    money=1000,
+)
 
-visit_hub(player)
-print("игра окончена")
+game = True
+while game:
+    visit_hub(player)
